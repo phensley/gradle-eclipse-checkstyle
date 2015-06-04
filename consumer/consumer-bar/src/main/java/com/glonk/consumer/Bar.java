c19ad3e2d3d9e190eb8ab3f7a7d2c1200e8444be
Patrick Hensley <pathensley@gmail.com>
2015-06-03 21:44:58 -0400

    Initial project boilerplate and consumer project.

---
+++ consumer/consumer-bar/src/main/java/com/glonk/consumer/Bar.java
@@ -0,0 +1,11 @@
+
+package com.glonk.consumer;
+
+public class Bar {
+
+  public static void main(String[] args) {
+    System.err.println("Bar");
+  }
+
+}
+
---
+++ consumer/consumer-foo/src/main/java/com/glonk/consumer/Foo.java
@@ -0,0 +1,12 @@
+
+package com.glonk.consumer;
+
+
+public class Foo {
+
+  public static void main(String[] args) {
+    System.err.println("Foo");
+  }
+
+}
+
